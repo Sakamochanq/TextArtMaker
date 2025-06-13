b881c46f55406d58322f13a7e4c8044196c058e1
Sakamochanq <Sakamochan.q@outlook.jp>
2025-06-13 22:37:17 +0900

    add: セピア調に変換する関数の追加

--- src/TextArtMaker/lib/ImageEdit.cs
+++ src/TextArtMaker/lib/ImageEdit.cs
@@ -37,5 +37,30 @@ namespace TextArtMaker
             }
             return bmp;
         }
+
+        public Bitmap SepiaTone(Image image)
+        {
+            Bitmap bmp = new Bitmap(image);
+
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color original = bmp.GetPixel(x, y);
+
+                    int tr = (int)(0.393 * original.R + 0.769 * original.G + 0.189 * original.B);
+                    int tg = (int)(0.349 * original.R + 0.686 * original.G + 0.168 * original.B);
+                    int tb = (int)(0.272 * original.R + 0.534 * original.G + 0.131 * original.B);
+
+                    // 値が255を超えないように調整
+                    int r = Math.Min(255, tr);
+                    int g = Math.Min(255, tg);
+                    int b = Math.Min(255, tb);
+
+                    bmp.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+            return bmp;
+        }
     }
 }
